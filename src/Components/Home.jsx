eaf4734d0fbe6cc4467b20d930bf07c817dccce0
Sathvika Taduru <sathvikataduru@gmail.com>
2025-07-01 14:24:24 +0530

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
-import image from "../images/sath1.jpg";
+import image from "../images/sath1.JPG";
+
 
 const Home = ({ name, title }) => {
   return (
