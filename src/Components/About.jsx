6e6c70e24975db7494e1b14d47cfeb32a86c0e84
Sathvika Taduru <sathvika.taduru@gmail.com>
2025-07-01 14:30:08 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import image from "../images/skillsbg.jpg";
+import image from "../images/skillsbg.jpeg";
 
 const imageAltText = "purple and blue abstract background";
 
